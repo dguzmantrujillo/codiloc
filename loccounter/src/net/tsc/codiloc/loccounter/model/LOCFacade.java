90b36b128aadc10a1975cf3aa8379bc4d596dabf
Diego.Guzman <dguzmantrujillo@gmail.com>
2014-04-13 10:18:05 -0500

    Commit

---
+++ loccounter/src/net/tsc/codiloc/loccounter/model/LOCFacade.java
@@ -0,0 +1,18 @@
+package net.tsc.codiloc.loccounter.model;
+
+/**
+ * 
+ * @author Diego.Guzman
+ *
+ */
+public class LOCFacade {
+
+	/**
+	 * 
+	 * @param sourceCode
+	 * @return
+	 */
+	public int count(String sourceCode) {
+		return 0;
+	}
+}
