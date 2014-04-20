8b57689a36aa44c8f06433bf8c6c3583d4eed223
CarolinaBenavides <carolinabc28@gmail.com>
2014-04-19 19:05:13 -0500

    Pruebas unitarias

--- CodilocTest/src/net/tsc/codiloc/versionmanager/model/VersionManagerFacadeTest.java
+++ CodilocTest/src/net/tsc/codiloc/versionmanager/model/VersionManagerFacadeTest.java
@@ -6,19 +6,20 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.tsc.codiloc.filemanager.exception.FileManagerException;
+import net.tsc.codiloc.filemanager.model.FileManagerFacade;
 import net.tsc.codiloc.loccomparator.exception.ComparatorException;
 import net.tsc.codiloc.loccomparator.model.ComparatorFacade;
 import net.tsc.codiloc.loccomparator.model.ComparedLine;
 import net.tsc.codiloc.versionmanager.exception.VersionManagerException;
-import net.tsc.codiloc.versionmanager.model.VersionManagerFacade;
 
 import org.junit.Test;
 
 public class VersionManagerFacadeTest {
 	
-	private final File original = new File("C:\\Pruebas\\original\\ComparedLine.java");
+	private final File original = new File("..\\CodilocTest\\testFiles\\original\\ComparedLine.java");
 	 
-    private final File modified = new File("C:\\Pruebas\\modificado\\ComparedLine.java");
+    private final File modified = new File("..\\CodilocTest\\testFiles\\modified\\ComparedLine.java");
     
 	@Test
 	public void compareVersion() throws VersionManagerException {
@@ -27,26 +28,30 @@ public class VersionManagerFacadeTest {
 		int deletedLines = 0;
 		int totalLines = 0;
 		
+		List<String> originalLines = null;
+		List<String> modifiedLines = null;
+		
 		List<ComparedLine> addedLinesList = new ArrayList<>();
 		List<ComparedLine> deletedLinesList = new ArrayList<>();
 
 		VersionManagerFacade versionManager = VersionManagerFacade.getInstance();
 		ComparatorFacade comparator = ComparatorFacade.getInstance();
+		FileManagerFacade fileManager = FileManagerFacade.getInstance();
 
-		// ADICIONAR FILEMANAGER
-		// Convertir archivos a listas de líneas
-		List<String> originalLines = new ArrayList<>();
-		List<String> modifiedLines = new ArrayList<>();
+		try {
+			originalLines = fileManager.getLinesFromFile(original);
+			modifiedLines = fileManager.getLinesFromFile(modified);
+		} catch (FileManagerException e) {
+			e.printStackTrace();
+		}
 
 		try {
 			addedLinesList = comparator.getAddedLOC(originalLines, modifiedLines);
-			addedLinesList = comparator.getAddedLOC(originalLines, modifiedLines);
-    		assertEquals("private String addLine;", addedLinesList.get(0).getTextLine());
+			assertEquals("private String addLine;", addedLinesList.get(0).getTextLine());
     		assertEquals("private int addLineNumber;", addedLinesList.get(1).getTextLine());
     		
 			deletedLinesList = comparator.getDeletedLOC(originalLines, modifiedLines);
-			deletedLinesList = comparator.getDeletedLOC(originalLines, modifiedLines);
-    		assertEquals("public void setTextLineNumber(int textLineNumber) {", deletedLinesList.get(0).getTextLine());
+			assertEquals("public void setTextLineNumber(int textLineNumber) {", deletedLinesList.get(0).getTextLine());
     		assertEquals("this.textLineNumber = textLineNumber;", deletedLinesList.get(1).getTextLine());
     		assertEquals("}", deletedLinesList.get(2).getTextLine());
 
@@ -56,9 +61,12 @@ public class VersionManagerFacadeTest {
 			deletedLines = versionManager.countComparedLOC(deletedLinesList);
 			assertEquals(3, deletedLines);
 			
-			totalLines = versionManager.countLOC("AQUI VA EL TEXTO DEL ARCHIVO MODIFICADO");
-			assertEquals(15, totalLines); //LA LLAVE QUE CIERRA Y ESTÁ SOLA EN LA LÍNEA CUENTA????
-
+			for(String line : modifiedLines){
+				totalLines += versionManager.countLOC(line);
+			}
+						
+			assertEquals(20, totalLines); 
+			
 		} catch (ComparatorException e) {
 			e.printStackTrace();
 		}
